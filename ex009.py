009ffbf8cdca46f836decc82f695391f775430cf
analuizanasc <analuiza_04@hotmail.com>
2022-04-20 10:17:57 -0300

    Revisão e aprimoramento do exercício

--- ex009.py
+++ ex009.py
@@ -10,3 +10,8 @@ n8 = n*8
 n9 = n*9
 n10 = n*10
 print(f'Sua tabuada é: \n {n}*1={n1} \n {n}*2={n2} \n {n}*3={n3} \n {n}*4={n4} \n {n}*5={n5} \n {n}*6={n6} \n {n}*7={n7} \n {n}*8={n8} \n {n}*9={n9} \n {n}*10={n10}')
+
+# outra forma de fazer a tabuada:
+print('Sua tabuada é:')
+for i in range(1,11):
+    print(f'{n} * {i} = {n*i}')
